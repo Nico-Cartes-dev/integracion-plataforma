1d4fc4ea54ef8ca6d0da516bb9e8453f1d61278a
TomasitoProgram <to.duhalde@duocuc.cl>
2025-06-13 21:37:53 -0400

    Webservice ActualizarGuiaDespacho

---
+++ BodegaBA-CSharp/BuenosAires.ServiceLayer/IWsActualizarGE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BuenosAires.ServiceLayer
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IWsActualizarGE" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IWsActualizarGE
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
